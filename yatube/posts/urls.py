cf10b94c6fc388f49a0e386f799f40d7d1ca90ec
Максим Иванов <ima_maxx@mail.ru>
2022-11-21 23:35:08 +0700

    Спринт 3/11 → Тема 5/7: База данных и Django ORM → Урок 5/6

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('',views.index),
-    path('group_list.html/', views.group_list),
+    path('',views.index, name = 'index'),
+    path('group_list.html/', views.group_list, name = 'group_list'),
 ]
